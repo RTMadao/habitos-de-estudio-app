16f636d391fe9c4cec1ed2f67b804cd1741b3ae2
Carlos Bello <bellocarlos2810@gmail.com>
2019-07-18 22:37:25 -0500

    metodo2

--- src/modelo/clases/SistemaNotificacion.ts
+++ src/modelo/clases/SistemaNotificacion.ts
@@ -3,11 +3,13 @@ import { Estudiante } from './Estudiante';
 
 export class SistemaNotificacion{
 
-    public anadirNotificacion(descripcion:String,fechaEnviado:Date,emisor:Estudiante, receptor:Padre){
+    public anadirNotificacion(descripcion:String,fechaEnviado:Date,emisor:Estudiante, receptor:Padre): boolean{
+        return true;
 
     }
 
-    public eliminarNotificacion(id:number){
+    public eliminarNotificacion(id:number): boolean{
+        return true;
 
     }
 
