da7b04a03033db9919bb5c03e6a7d52f5d29ae58
JustAPeter <47680127+JustAPeter@users.noreply.github.com>
2019-07-19 23:31:59 -0500

    signIn creado
    Faltan ajustes

--- src/vista/views/inicio.vue
+++ src/vista/views/inicio.vue
@@ -1,17 +1,28 @@
 <template>
-    <navbarOut/>
+    <v-container>
+        <navbarOut/>
+        <signIn xs6 xl5 />
+    </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import navbarOut from '@/vista/components/navbar_out.vue'
+import signIn from '@/vista/components/signIn.vue'
 
 @Component({
     components:{
-        navbarOut
+        navbarOut,
+        signIn
     }
 })
 
-
 export default class inicio extends Vue {}
 </script>
+
+<style scoped>
+    signIn{
+        height: 75%;
+        
+    }
+</style>
