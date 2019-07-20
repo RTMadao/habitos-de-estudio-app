7c277110d3b5ccda14b370e7b293bb59ce8cc296
Carlos Bello <bellocarlos2810@gmail.com>
2019-07-19 22:26:30 -0500

    controladores de vista modelo y persistencia 1

--- src/persistencia/padrePersistencia.ts
+++ src/persistencia/padrePersistencia.ts
@@ -1,16 +1,19 @@
 import { Padre } from '@/modelo/clases/Padre';
 
 export class PadrePersistencia{
-    public guardarPadreBD(padre: Padre): boolean{
+    public guardar(padre: Padre){
         return true;
     }
-    public buscarPadreBD(nombreUsuario: String): Padre{
+    public buscar(nombreUsuario: String): Padre{
         return new Padre("","","");
     }
-    public autentificarPadreBD(nombreUsuario:String,contrasena:String): boolean{
+    public autentificar(nombreUsuario:String,contrasena:String): boolean{
         return true;
     }
-    public actualizarPadre(padre:Padre): boolean{
+    public actualizar(padre:Padre): boolean{
+        return true;
+    }
+    public validar(padre: Padre): boolean{
         return true;
     }
 }
