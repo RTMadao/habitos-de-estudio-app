ed47951467e7c4ed1833fcd9b90f4d2cfee0a260
JustAPeter <47680127+JustAPeter@users.noreply.github.com>
2019-07-23 21:46:23 -0500

    Eliminar proyecto
    Para cambios en el lenguaje utilizado

--- src/modelo/clases/Recomendacion.ts
+++
@@ -1,9 +0,0 @@
-export class Recomendacion{
-    private descripcion:String;
-    private escenarioUso:String;
-
-    constructor(descripcion:String,escenarioUso:String){
-        this.descripcion = descripcion;
-        this.escenarioUso = escenarioUso;
-    }
-}
