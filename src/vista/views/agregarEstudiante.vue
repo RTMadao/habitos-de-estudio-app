32de1e95fc3d9135b2f04e7a42b4468657e3adc3
JustAPeter <47680127+JustAPeter@users.noreply.github.com>
2019-07-27 21:17:02 -0500

    Agregar estudiante añadido
    -version 0.4-

---
+++ src/vista/views/agregarEstudiante.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-container fluid pa-0 justify-center align-center>
+        <v-app-bar app style=" background-color:#0099cc">
+            <v-icon class="fas fa-book-open" style="font-size:30px; color:#FFFE03" />
+            <v-spacer></v-spacer>
+            <v-btn text @click="toIniciarSesion()"> <span class="fas fa-arrow-left"/> Inicia sesión</v-btn>
+        </v-app-bar>
+        <v-layout wrap mt-5 justify-center style="height:85%" >
+            <v-flex xs10 md4 class="elevation-14" style="background-color:#a3f799">
+                <v-carousel hide-delimiters hide-delimiter-background show-arrows-on-hover>
+                    <v-carousel-item>
+                        <v-layout align-center fill-height justify-center>
+                            <v-img src="https://www.ecured.cu/images/thumb/a/a4/Alpaca.jpg/260px-Alpaca.jpg" alt="img 1" />
+                        </v-layout>
+                    </v-carousel-item>
+                    <v-carousel-item>
+                        <v-layout justify-center fill-height align-center>
+                            <div class="display-3"> HEEEEY </div>
+                        </v-layout>
+                    </v-carousel-item>
+                    <v-carousel-item>
+                        <v-layout justify-center fill-height align-center>
+                            <div class="display-2"> Para las recomendaciones :B </div>
+                        </v-layout>
+                    </v-carousel-item>
+                </v-carousel>
+            </v-flex>
+            <v-flex xs10 md4 style="background-color:#c2f5bc" class="elevation-14" justify-center align-center>
+                <v-layout mt-5 class="formRegistro">
+                    <v-layout justify-center align-center class="formcontainer" column>
+                        <v-flex mt-3 justify-center align-center><div class="headline"> <v-icon class="fas fa-child" style="color:black"/> AGREGA UN ESTUDIANTE</div> <br> </v-flex>
+                        <v-flex><input type="text" placeholder="Nombre"> </v-flex>
+                        <v-flex><input type="text" placeholder="Nombre de Usuario"> </v-flex>
+                        <v-flex><input type="password" placeholder="Contraseña"> </v-flex>
+                        <v-flex><input type="password" placeholder="Confirmar Contraseña"> </v-flex>
+                        <v-flex><input class="button" type="submit" value="Agregar estudiante"> </v-flex>
+                    </v-layout>
+                </v-layout>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.formRegistro{
+    height:90%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+
+input{
+    font-size: 20px;
+    width: 300px;
+    margin: 0px;
+    margin-top: 5%;
+    align-content: center;
+    text-align: center;
+    padding: 4px 0px;
+    border-radius:5px;
+    border: 1px solid darkgray;
+    background-color: white;
+}
+.button{
+    background-color: #ffcc33; /*FFFE03*/
+    display: inline-block;
+    margin-bottom: 6px;
+    color: black;
+    padding: 4px 0px;
+    width: 200px;
+    text-decoration: none;
+    border: 1px solid darkgray;
+}
+</style>
