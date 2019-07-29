367eb09aa71cef000be1712bb5bb7deb2b94fe86
JustAPeter <47680127+JustAPeter@users.noreply.github.com>
2019-07-28 22:21:47 -0500

    Update inicioEstudiante.vue
    Correccion variables

--- src/vista/views/inicioEstudiante.vue
+++ src/vista/views/inicioEstudiante.vue
@@ -21,14 +21,14 @@
                                     <v-flex px-2 xs11 style="max-height:30px" v-for="(tarea,index) in tareas" v-bind:key="index">
                                         <v-flex v-if="tarea.hecha" >
                                             <v-flex v-if="tarea.verificada">
-                                                <div style="color:grey; text-decoration:line-through;"> [{{tarea.fecha}} ] {{tarea.tipoActividad}} en {{tarea.materia}} </div>
+                                                <div style="color:grey; text-decoration:line-through;"> [{{tarea.fecha}} ] {{tarea.descripcion}} en {{tarea.materia}} </div>
                                             </v-flex>
                                             <v-flex v-else>
-                                                <div style="color:grey;"> [{{tarea.fecha}} ] {{tarea.tipoActividad}} en {{tarea.materia}} (Revisar) </div>
+                                                <div style="color:grey;"> [{{tarea.fecha}} ] {{tarea.descripcion}} en {{tarea.materia}} (Revisar) </div>
                                             </v-flex>
                                         </v-flex>
                                         <v-flex v-else>
-                                            <div style="color:black;"> [{{tarea.fecha}} ] {{tarea.tipoActividad}} en {{tarea.materia}} </div>
+                                            <div style="color:black;"> [{{tarea.fecha}} ] {{tarea.descripcion}} en {{tarea.materia}} </div>
                                         </v-flex>
                                     </v-flex>
                                 </v-layout>
@@ -42,8 +42,7 @@
                         <v-layout xs8 justify-center align-center class="formcontainer"  column>
                             <v-flex mt-3 justify-center align-center><div class="headline"> <v-icon class="fas fa-tasks" style="color:black"/> AGREGA UNA TAREA</div> <br> </v-flex>
                             <v-flex><input v-model="tareaAgregar.fecha" type="date" placeholder="Fecha"> </v-flex>
-                            <v-flex mt-3> <textarea rows="3" cols="23" style="background-color:white; border-radius: 4px 4px 4px 4px; -moz-border-radius: 4px 4px 4px 4px; -webkit-border-radius: 4px 4px 4px 4px; border: 1px solid darkgrey" placeholder="Descripcion"> </textarea> </v-flex>
-                            <!--<v-flex><input v-model="actividad" type="-" > </v-flex>-->
+                            <v-flex mt-3> <textarea v-model="tareaAgregar.descripcion" rows="3" cols="23" style="background-color:white; border-radius: 4px 4px 4px 4px; -moz-border-radius: 4px 4px 4px 4px; -webkit-border-radius: 4px 4px 4px 4px; border: 1px solid darkgrey" placeholder="Descripcion"> </textarea> </v-flex>
                             <v-flex><input v-model="tareaAgregar.materia" type="text" placeholder="Materia"> </v-flex>
                             <v-flex><input class="button" type="submit" value="Agregar" style="font-color:yellow;border: 1px solid yellow; background-color: white;"> </v-flex>
                         </v-layout> 
@@ -61,21 +60,21 @@ export default {
             selActividades:['investigación','taller','examen'],
             tareaAgregar:{
                 fecha:'',
-                actividad:'',
+                descripcion:'',
                 materia:''
             },
             tareas:[
                 {
                     fecha: '27-07-2019',
                     materia:'economia',
-                    tipoActividad: 'Expo',
+                    descripcion: 'Expo',
                     hecha: false,
                     verificada: false
                 },
                 {
                     fecha: '27-07-2019',
                     materia:'dibujo',
-                    tipoActividad:'Proyecto',
+                    descripcion:'Proyecto',
                     hecha: true,
                     verificada: true
                 }
